81f72c4fee848a756cc98ab56bbce9eed25a91e8
rory0423 <132328126+rory0423@users.noreply.github.com>
2024-05-28 22:50:28 +0900

    Add files via upload

---
+++ Week12/Lecture12-HW/Lecture12-HW.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main(void)
+{
+
+	using namespace std::chrono;
+	constexpr seconds interval(1);  // 1초 간격
+
+	auto next_frame = steady_clock::now() + interval;
+
+	while (true)
+	{
+		auto start = steady_clock::now();
+
+		// 다음 프레임 시간까지 대기
+		std::this_thread::sleep_until(next_frame);
+		next_frame += interval;
+
+		auto end = steady_clock::now();
+		auto elapsed = duration_cast<milliseconds>(end - start);
+		std::cout << "Elapsed time for loop: " << elapsed.count() << " ms" << std::endl;
+	}
+	return 0;
+}
